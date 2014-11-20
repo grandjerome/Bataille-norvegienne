30e605ad300918b6161fa287f43c2e1018ad91cd
grandjerome <aladune@gmail.com>
2014-11-20 01:09:56 +0100

    Update JoueurVirtuel.java
    constructeur joueur virtuel

--- JoueurVirtuel.java
+++ JoueurVirtuel.java
@@ -1,7 +1,79 @@
 package Moteur;
 
-public class JoueurVirtuel extends Moteur.Joueur  {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class JoueurVirtuel {
+
+	/*
+	 * (non-javadoc)
+	 */
+	private int nbCartes;
+
+	/**
+*
+*/
+
+	private Partie partie = null;
 	
+	private List<Carte> main;
+	private List<Carte> faceCachee;
+	private List<Carte> faceVisible;
 	
+	public JoueurVirtuel(){
+		
 	}
+	public void echangerCarte() {
+
+	}
+
+	public void poserCarte() {
+
+	}
+
+	/**
+	 * Getter of the property <tt>nbCartes</tt>
+	 * 
+	 * @return Returns the nbCartes.
+	 * 
+	 */
+
+	public int getNbCartes() {
+		return nbCartes;
+	}
+
+	/**
+	 * Setter of the property <tt>nbCartes</tt>
+	 * 
+	 * @param nbCartes
+	 *            The nbCartes to set.
+	 * 
+	 */
+	public void setNbCartes(int nbCartes) {
+		this.nbCartes = nbCartes;
+	}
+
+	/**
+	 * Getter of the property <tt>main</tt>
+	 * 
+	 * @return Returns the main.
+	 * 
+	 */
+
+	
+	/**
+	 * Setter of the property <tt>main</tt>
+	 * 
+	 * @param main
+	 *            The main to set.
+	 * 
+	 */
+	
+
+	
+
+}
+
 
